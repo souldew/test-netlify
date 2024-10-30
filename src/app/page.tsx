382a0ed7a8ae4c655bc0818e904a6436f3b74427
souldew <souldew2002@gmail.com>
2024-10-31 01:06:16 +0900

    change backgroud

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div className="mt-auto mb-auto bg-green-200 justify-center items-center">
+      <div className="mt-auto mb-auto bg-blue-200 justify-center items-center">
         <div>Hello World! netlify test page</div>
         <div>
           <button>Button</button>
